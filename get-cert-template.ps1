ee93ace97892d254e9e17ae4754805b13ed602ec
11010490 <106936112+11010490@users.noreply.github.com>
2023-07-27 17:10:01 +1000

    Update get-cert-template.ps1

--- get-cert-template.ps1
+++ get-cert-template.ps1
@@ -4,9 +4,17 @@ Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object { $_.Thumbprint -eq $th
 $certificate = Get-ChildItem -Path Cert:\LocalMachine\My | Select-Object -First 1
 $templateName = $certificate.Extensions | Where-Object { $_.Oid.FriendlyName -eq "Certificate Template Information" } | Select-Object -ExpandProperty Format(1)
 
-if ($templateName -ne $null) {
-    $templateName = $templateName.Substring(26)
-    Write-Host "Certificate Template Name: $templateName"
+# Check if the certificate was found
+if ($certificate -eq $null) {
+    Write-Host "Certificate with thumbprint $thumbprint not found."
 } else {
-    Write-Host "Certificate template information not found for the certificate."
+    # Get the certificate template information
+    $templateInfo = $certificate.Extensions | Where-Object { $_.Oid.FriendlyName -eq "Certificate Template Information" }
+    if ($templateInfo -ne $null) {
+        # Extract the template name from the format string
+        $templateName = $templateInfo.Format(1)
+        Write-Host "Certificate Template Name: $templateName"
+    } else {
+        Write-Host "Certificate template information not found for the certificate."
+    }
 }
