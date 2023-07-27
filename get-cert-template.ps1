5e7f55c1552fbfb63d2a0be47af66e7b434d720f
11010490 <106936112+11010490@users.noreply.github.com>
2023-07-27 17:16:18 +1000

    Update get-cert-template.ps1

--- get-cert-template.ps1
+++ get-cert-template.ps1
@@ -1,20 +1,12 @@
-$thumbprint = '1123123131213321'
-Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object { $_.Thumbprint -eq $thumbprint }
+# List certificates installed in the LocalMachine store
+$certs = Get-ChildItem -Path Cert:\LocalMachine\My
 
-$certificate = Get-ChildItem -Path Cert:\LocalMachine\My | Select-Object -First 1
-$templateName = $certificate.Extensions | Where-Object { $_.Oid.FriendlyName -eq "Certificate Template Information" } | Select-Object -ExpandProperty Format(1)
-
-# Check if the certificate was found
-if ($certificate -eq $null) {
-    Write-Host "Certificate with thumbprint $thumbprint not found."
-} else {
-    # Get the certificate template information
-    $templateInfo = $certificate.Extensions | Where-Object { $_.Oid.FriendlyName -eq "Certificate Template Information" }
-    if ($templateInfo -ne $null) {
-        # Extract the template name from the format string
-        $templateName = $templateInfo.Format(1)
-        Write-Host "Certificate Template Name: $templateName"
-    } else {
-        Write-Host "Certificate template information not found for the certificate."
+# Loop through each certificate and display its template name
+foreach ($cert in $certs) {
+    $templateName = $cert.Extensions | Where-Object { $_.Oid.FriendlyName -eq "Certificate Template Name" } | ForEach-Object { $_.Format(0) }
+    if ($templateName) {
+        Write-Host "Certificate Subject: $($cert.Subject)"
+        Write-Host "Template Name: $templateName"
+        Write-Host "--------------------------"
     }
 }
